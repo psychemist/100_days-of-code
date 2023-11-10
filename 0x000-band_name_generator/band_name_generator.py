ef0f07d8b3b02da8d96fefc0fb7d558ed4c7b53f
psych0wh1z <ikeagudike@gmail.com>
2023-11-10 23:58:42 +0100

    Add first task file

--- 0x000-band_name_generator/band_name_generator.py
+++ 0x000-band_name_generator/band_name_generator.py
@@ -1,10 +1,10 @@
-#1. Create a greeting for your program.
-
-#2. Ask the user for the city that they grew up in.
-
-#3. Ask the user for the name of a pet.
-
-#4. Combine the name of their city and pet and show them their band name.
-
-#5. Make sure the input cursor shows on a new line:
-
+# Create greeting
+print("Welcome to Band Generator!")
+# Ask user for city they grew up in
+city = input("What city did you grow up in? ")
+# Ask user for name of a pet
+pet = input("What was the name of your first pet? ")
+# Combine name of city and pet and show band name
+band_name = f"{city} {pet} Band"
+# Ensure input cursor shows on new line
+print(band_name)
