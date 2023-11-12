5e717b3f6e130fd87c5d3508f834b4439ee4904e
psych0wh1z <ikeagudike@gmail.com>
2023-11-12 14:00:00 +0100

    Fix input commands

--- 0x000-band_name_generator/band_name_generator.py
+++ 0x000-band_name_generator/band_name_generator.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+
 # Create greeting
-print("Welcome to Band Generator!")
+print("Welcome to the Band Name Generator!")
 # Ask user for city they grew up in
-city = input("What city did you grow up in? ")
+city = input("What city did you grow up in?\n")
 # Ask user for name of a pet
-pet = input("What was the name of your first pet? ")
+pet = input("What was the name of your first pet?\n")
 # Combine name of city and pet and show band name
 band_name = f"{city} {pet} Band"
 # Ensure input cursor shows on new line
-print(band_name)
+print(f"Your band name could be {band_name}")
